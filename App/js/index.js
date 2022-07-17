0e2df1f9c8de897aa90aaed251c1ed93144170e5
nazir-app <109414928+nazir-app@users.noreply.github.com>
2022-07-17 16:26:09 +0600

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -27,7 +27,7 @@ xui.Class('App', 'xui.Module',{
                 .setLeft("9.904761904761905em")
                 .setTop("2.2857142857142856em")
                 .setWidth("32.91428571428571em")
-                .setHeight("2.742857142857143em")
+                .setHeight("2.125em")
                 .setCaption("Timing & Consumption")
                 .setHAlign("center")
                 .setFontColor("#000000")
@@ -54,11 +54,12 @@ xui.Class('App', 'xui.Module',{
             append(
                 xui.create("xui.UI.Label")
                 .setHost(host,"xui_ui_label23")
-                .setDock("left")
-                .setLeft("2.2857142857142856em")
-                .setTop("3.0476190476190474em")
-                .setWidth("3.123809523809524em")
+                .setLeft("0em")
+                .setTop("3.8095238095238093em")
+                .setWidth("3.8857142857142857em")
+                .setHeight("2.057142857142857em")
                 .setCaption("Date:")
+                .setHAlign("center")
                 .setFontColor("#000000")
                 .setFontSize("14px")
                 .setFontFamily("arial")
@@ -67,12 +68,53 @@ xui.Class('App', 'xui.Module',{
             append(
                 xui.create("xui.UI.ComboInput")
                 .setHost(host,"xui_ui_comboinput750")
-                .setLeft("3.0476190476190474em")
-                .setTop("3.0476190476190474em")
+                .setLeft("3.8095238095238093em")
+                .setTop("3.8095238095238093em")
                 .setWidth("8.076190476190476em")
+                .setHeight("1.6761904761904762em")
                 .setLabelSize("8em")
-                .setLabelCaption("Date ")
                 .setType("date")
+                .setValue("1656784800000")
+                .setCustomStyle({
+                    "KEY" : {
+                    },
+                    "BORDER" : {
+                        "border-top" : "solid 1px #000000",
+                        "border-right" : "solid 1px #000000",
+                        "border-bottom" : "solid 1px #000000",
+                        "border-left" : "solid 1px #000000",
+                        "text-align" : "center"
+                    }
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label20")
+                .setLeft("0.7619047619047619em")
+                .setTop("6.857142857142857em")
+                .setCaption("Buyer:")
+                .setHAlign("left")
+                .setFontSize("14px")
+                .setFontWeight("bold")
+            );
+            
+            append(
+                xui.create("xui.UI.CSSBox")
+                .setHost(host,"xui_ui_cssbox3")
+                .setClassName("xui-css-aja")
+            );
+            
+            append(
+                xui.create("xui.UI.CSSBox")
+                .setHost(host,"xui_ui_cssbox4")
+                .setClassName("xui-css-ajs")
+            );
+            
+            append(
+                xui.create("xui.UI.CSSBox")
+                .setHost(host,"xui_ui_cssbox5")
+                .setClassName("xui-css-akk")
             );
             
             return children;
@@ -83,6 +125,19 @@ xui.Class('App', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
+        },
+        events:{
+        },
+        _page_onready:function(e,i){
+
+        },
+        /**
+         * Fired when the control was rendered
+         * @method onRender [xui.Module event]
+         * @param {xui.UIProfile.} profile  The current control's profile object
+        */
+        _page_onrender:function(profile){
+
         }
         /*,
         // To determine how properties affects this module
