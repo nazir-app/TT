d41e6ae096b0afd94fca8de942b6f22a533198a6
nazir-app <109414928+nazir-app@users.noreply.github.com>
2022-07-17 15:00:28 +0600

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,62 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label13")
+                .setDock("top")
+                .setLeft("9.904761904761905em")
+                .setTop("2.2857142857142856em")
+                .setWidth("32.91428571428571em")
+                .setHeight("2.742857142857143em")
+                .setCaption("Timing & Consumption")
+                .setHAlign("center")
+                .setFontColor("#000000")
+                .setFontSize("18px")
+                .setFontFamily("arial")
+                .setCustomStyle({
+                    "KEY" : {
+                        "background-color" : "#A0C3DF",
+                        "background-position" : "center center",
+                        "font-family" : "arial black,avant garde",
+                        "font-size" : "16px"
+                    },
+                    "VALIGN" : {
+                        "text-align" : "center"
+                    },
+                    "CAPTION" : {
+                        "font-weight" : "bold",
+                        "font-style" : "normal",
+                        "text-align" : "center"
+                    }
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label23")
+                .setDock("left")
+                .setLeft("2.2857142857142856em")
+                .setTop("3.0476190476190474em")
+                .setWidth("3.123809523809524em")
+                .setCaption("Date:")
+                .setFontColor("#000000")
+                .setFontSize("14px")
+                .setFontFamily("arial")
+            );
+            
+            append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput750")
+                .setLeft("3.0476190476190474em")
+                .setTop("3.0476190476190474em")
+                .setWidth("8.076190476190476em")
+                .setLabelSize("8em")
+                .setLabelCaption("Date ")
+                .setType("date")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
